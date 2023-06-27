390b68e3fd041dc004e84d3d3a0fb89939b9aa90
Monica "Araceli" Kyne <109679917+Aracelikyne@users.noreply.github.com>
2023-06-27 09:41:23 -0500

    Ticket 1

---
+++ src/main/java/com/hcc/entities/Assignment.java
@@ -0,0 +1,14 @@
+package com.hcc.entities;
+
+public class Assignment {
+
+    Long id;
+    String status;
+    int number;
+    String githubURL;
+    String branch;
+    String reviewVideoUrl;
+    User user;
+    User codeReviewer;
+
+}
